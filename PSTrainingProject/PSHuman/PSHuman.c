0f3dce61e77d39a5e583f4ce60ae24bca8949ef3
Сергей <sergbob84@gmail.com>
2015-10-07 20:18:55 +0300

    add .guiding

---
+++ PSTrainingProject/PSHuman/PSHuman.c
@@ -0,0 +1,9 @@
+//
+//  PSHuman.c
+//  PSTrainingProject
+//
+//  Created by Сергей on 05.10.15.
+//  Copyright © 2015 Сергей. All rights reserved.
+//
+
+#include "PSHuman.h"
---
+++ PSTrainingProject/PSHuman/PSHuman.h
@@ -0,0 +1,14 @@
+//
+//  PSHuman.h
+//  PSTrainingProject
+//
+//  Created by Сергей on 05.10.15.
+//  Copyright © 2015 Сергей. All rights reserved.
+//
+
+#ifndef PSHuman_h
+#define PSHuman_h
+
+#include <stdio.h>
+
+#endif /* PSHuman_h */

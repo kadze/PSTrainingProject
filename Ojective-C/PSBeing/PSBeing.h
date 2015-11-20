1ecb02d3109a138d4a06ba12b05672d056bf0e26
Сергей <sergbob84@gmail.com>
2015-11-20 18:37:05 +0200

    Add Being

---
+++ Ojective-C/PSBeing/PSBeing.h
@@ -0,0 +1,13 @@
+//
+//  PSBeing.h
+//  PSTrainingProject
+//
+//  Created by Сергей on 20.11.15.
+//  Copyright © 2015 Сергей. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSBeing : NSObject
+
+@end
---
+++ Ojective-C/PSBeing/PSBeing.m
@@ -0,0 +1,13 @@
+//
+//  PSBeing.m
+//  PSTrainingProject
+//
+//  Created by Сергей on 20.11.15.
+//  Copyright © 2015 Сергей. All rights reserved.
+//
+
+#import "PSBeing.h"
+
+@implementation PSBeing
+
+@end
